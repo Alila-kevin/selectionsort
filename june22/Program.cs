a75fd30ae4631427cd75f0f0ab4334a02069566a
unknown <Administrator@ICTINTERN-PC.Billserver.kiwasco.co.ke>
2023-06-22 12:23:05 +0300

    selection sort

---
+++ june22/Program.cs
@@ -0,0 +1,36 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //Console.WriteLine("Hello, World!");
+        int[] number = {100, 31, 4, 13, 78, 56, 37, 76};
+        for (int i=0; i < number.Length; i++)
+        {
+            Console.WriteLine(number[i]);
+        }
+        selectionsort(number);
+        foreach (char digit in number)
+        {
+            Console.WriteLine(digit);
+        }
+
+        Console.ReadKey();
+    }
+    private static void selectionsort(int[] number)
+    {
+        for(int i=0; i < number.Length; i++)
+        {
+            int min = i;
+            for (int j=0; j<number.Length; j++)
+            {
+                if (number[min] > number[j])
+                {
+                    min = j;
+                }
+            } ;
+            int temp = number[i];
+            number[i] = number[min];
+            number[min] = temp;
+        }
+    }
+}
